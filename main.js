96110bf8fd405f094c20471d2a1e838dbb265fe0
dostonwebdev <dostone320@gmail.com>
2023-10-09 12:02:29 +0500

    hom

---
+++ main.js
@@ -0,0 +1,48 @@
+//  1 masala
+
+// let a = 7;
+// a++;
+// console.log(a);
+
+//  2 masala
+
+// let a = 5;
+// a -= 2;
+// console.log(a);
+
+// 3 masala
+
+// boolean 1
+
+// let a = 10;
+// let res = a > 0;
+// console.log(res);
+
+// boolean 2
+
+// let a = 11;
+// let res = a > 0;
+// console.log(res);
+
+// bolean 4
+
+// let a = 4;
+// let b = 2;
+// let res = a > 2 && b <= 3;
+// console.log(res);
+
+// bolean 5
+
+// let a = 4;
+// let b = 1;
+// let res = a > 0 && b <= 2;
+// console.log(res);
+
+// boolena 6
+
+// let a = 5;
+// let b = 6;
+// let c = 8;
+// let res = a <= b && b<=c;
+// console.log(res);
+
